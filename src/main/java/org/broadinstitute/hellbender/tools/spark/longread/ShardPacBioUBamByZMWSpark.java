cc7c6f56797dc666fc8a8baf62ed994c31526159
Steve Huang <shuang@broadinstitute.org>
2020-03-19 22:05:16 -0400

    new tool ShardPacBioUBamByZMWSpark

---
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/longread/ShardPacBioUBamByZMWSpark.java
@@ -0,0 +1,13 @@
+package org.broadinstitute.hellbender.tools.spark.longread;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.broadinstitute.hellbender.engine.spark.GATKSparkTool;
+
+public class ShardPacBioUBamByZMWSpark extends GATKSparkTool {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void runTool( final JavaSparkContext ctx ) {
+
+    }
+}
